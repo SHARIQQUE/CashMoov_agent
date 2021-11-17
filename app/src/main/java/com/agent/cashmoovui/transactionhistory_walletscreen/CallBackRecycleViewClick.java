1eb3efa708bcc6f816e45ee4280ae8c24342ca1d
sharique <sharique9718@gmail.com>
2021-11-17 20:45:31 +0530

    SHARIQUE - AIRTIME PURCHASE-   - 17 Nov 2021

---
+++ app/src/main/java/com/agent/cashmoovui/transactionhistory_walletscreen/CallBackRecycleViewClick.java
@@ -0,0 +1,11 @@
+package com.agent.cashmoovui.transactionhistory_walletscreen;
+
+
+import com.agent.cashmoovui.model.UserDetailAgent;
+
+public interface CallBackRecycleViewClick {
+
+
+      void  callBackReycleView(String  code_walletOwnerChildList);
+
+}
