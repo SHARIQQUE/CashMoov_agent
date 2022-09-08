70e88a57a705256ad92601277d4308af420b13b0
User <sharique9718@gmail.com>
2022-09-08 16:25:34 +0530

    bugs fixed (08-09-2022)

--- app/src/main/java/com/agent/cashmoovui/remittancebyabhay/international/InternationalRemittanceConfirmScreen.java
+++ app/src/main/java/com/agent/cashmoovui/remittancebyabhay/international/InternationalRemittanceConfirmScreen.java
@@ -208,6 +208,7 @@ public class InternationalRemittanceConfirmScreen extends AppCompatActivity impl
 
 
 
+
                     tvSenderCode.setText(InternationalRemittanceSenderKYC.sendorCustomerJsonObj.optJSONObject("customer").optString("code"));
                     tvBenefiCode.setText(InternationalRemittanceBenefiKYC.benefiCustomerJsonObj.optJSONObject("customer").optString("code"));
 
